3f6f73d3915d676297c84448f4a3c14eb8751131
mgblanco10 <mgblanco1001@gmail.com>
2023-05-31 08:30:17 +0200

    wip

---
+++ fideua/index.js
@@ -0,0 +1,15 @@
+
+function calcularFideua(){
+    const fideosPorPersona = 125;
+    const calamaresPorPersona = 100;
+    const gambasPorPersona = 50;
+    const precioFideos = parseFloat(document.getElementById("fideos").value)
+    const precioCalamares = parseFloat(document.getElementById("calamares").value)
+    const precioGambas = parseFloat(document.getElementById("gambas").value)
+    const personas = parseInt(document.getElementById("personasQueComen").value)
+    
+    
+    console.log(precioFideos)
+}
+
+document.getElementById("resultado1").innerHTML = "suma"
